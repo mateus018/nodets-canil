78e526c78e2e9cf019e5e292b4d78f0800bbed19
Mateus do Nascimento Rodrigues <mateus.nascimento018@gmail.com>
2025-03-02 00:55:09 -0300

    heroku setup - add tsc

--- src/server.ts
+++ src/server.ts
@@ -20,6 +20,8 @@ server.use(express.static(path.join(__dirname,'../public')));
  server.use(mainRoutes);
 
  server.use((req,res)=>{
-    res.render('pages/404')
+    res.render('pages/404');
  });
+
+ 
 server.listen(process.env.Port);
